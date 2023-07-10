157c07ec607e5f000283805fa5b7f8432baf11c2
Matt Favazza <mmf4435@rit.edu>
2023-07-10 16:29:28 -0400

    Matt Favazza - create REST endpoint for reporting structure

---
+++ src/main/java/com/mindex/challenge/controller/ReportingStructureController.java
@@ -0,0 +1,26 @@
+package com.mindex.challenge.controller;
+
+import com.mindex.challenge.data.ReportingStructure;
+import com.mindex.challenge.service.ReportingStructureService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ReportingStructureController {
+
+    private static final Logger LOG = LoggerFactory.getLogger(ReportingStructureController.class);
+
+    @Autowired
+    private ReportingStructureService reportingStructureService;
+
+    @RequestMapping("/reportingStructure/{id}")
+    public ReportingStructure request(@PathVariable String id){
+        LOG.debug("Received employee reporting structure create request for id [{}]", id);
+
+        return reportingStructureService.request(id);
+    }
+}
---
+++ src/main/java/com/mindex/challenge/service/impl/ReportingStructureServiceImpl.java
@@ -0,0 +1,32 @@
+package com.mindex.challenge.service.impl;
+
+import com.mindex.challenge.dao.EmployeeRepository;
+import com.mindex.challenge.data.Employee;
+import com.mindex.challenge.data.ReportingStructure;
+import com.mindex.challenge.service.ReportingStructureService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ReportingStructureServiceImpl implements ReportingStructureService {
+
+    private static final Logger LOG = LoggerFactory.getLogger(ReportingStructureServiceImpl.class);
+
+    @Autowired
+    private EmployeeRepository employeeRepository;
+
+    @Override
+    public ReportingStructure request(String id) {
+        LOG.debug("Creating employee with id [{}]", id);
+
+        Employee employee = employeeRepository.findByEmployeeId(id);
+
+        if (employee == null) {
+            throw new RuntimeException("Invalid employeeId: " + id);
+        }
+
+        return new ReportingStructure(employee);
+    }
+}
